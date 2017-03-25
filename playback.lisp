4df8c0fe9197395db979ece3a73ccf3e6c50ca1f
peyton <peyton@peytonfarrar.com>
2017-03-24 21:20:12 -0800

    Actually revert "Revert: Added types to playback functions"
    This reverts commit 452a593bacea6bd5f6a84299b1d8724c5f5e4145.

--- playback.lisp
+++ playback.lisp
@@ -17,38 +17,31 @@
 
 (defun playback-consume (socket state)
   "Sets the consume state, either t or nil."
-  (declare (type boolean state))
-  (send-command socket "consume" (boolean->binary (the boolean state))))
+  (send-command socket "consume" (boolean->binary state)))
 
 (defun playback-crossfade (socket seconds)
   "Sets crossfading between songs in seconds."
-  (declare (type unsigned-byte seconds))
-  (send-command socket "crossfade" (the unsigned-byte seconds)))
+  (send-command socket "crossfade" seconds))
 
 (defun playback-mixrampdb (socket decibels)
   "Sets the threshold at which songs will be overlapped."
-  (declare (type signed-byte decibels))
-  (send-command socket "mixrampdb" (the signed-byte decibels)))
+  (send-command socket "mixrampdb" decibels))
 
 (defun playback-mixrampdelay (socket seconds)
   "Additional time subtracted from the overlap calculated by mixrampdb."
-  (declare (type unsigned-byte seconds))
-  (send-command socket "mixrampdelay" (the unsigned-byte seconds)))
+  (send-command socket "mixrampdelay" seconds))
 
 (defun playback-random (socket state)
   "Sets the random state to t or nil."
-  (declare (type boolean state))
-  (send-command socket "random" (boolean->binary (the boolean state))))
+  (send-command socket "random" (boolean->binary state)))
 
 (defun playback-set-volume (socket volume)
   "Sets the volume, the range of volume is 0-100."
-  (declare (type signed-byte volume))
-  (send-command socket "setvol" (the signed-byte volume)))
+  (send-command socket "setvol" volume))
 
 (defun playback-single (socket state)
   "Sets the single state to t or nil."
-  (declare (type boolean state))
-  (send-command socket "single" (boolean->binary (the boolean state))))
+  (send-command socket "single" (boolean->binary state)))
 
 (defun replay-gain-mode (socket gain-mode)
   "Sets the replay gain mode. Mode must be one of off, track, ablum, or auto."
@@ -66,8 +59,7 @@
 
 (defun playback-pause (socket state)
   "Sets the pause state to t or nil"
-  (declare (type boolean state))
-  (send-command socket "pause" (boolean->binary (the boolean state))))
+  (send-command socket "pause" (boolean->binary state)))
 
 (defun playback-play (socket &optional song-position)
   "Begins playing the playlist at song number song-position."
@@ -84,21 +76,16 @@
 (defun playback-seek (socket song-position time)
   "Seeks to the position time (in seconds) of entry song-position in the
   playlist."
-  (declare (type unsigned-byte song-position time))
-  (send-command socket "seek" (the unsigned-byte song-position)
-                (the unsigned-byte time)))
+  (send-command socket "seek" song-position time))
 
 (defun playback-seek-id (socket song-id time)
   "Seeks to the position time (in seconds) of song song-id."
-  (declare (type unsigned-byte song-id time))
-  (send-command socket "seekid" (the unsigned-byte song-id)
-                (the unsigned-byte time)))
+  (send-command socket "seekid" song-id time))
 
 (defun playback-seek-current (socket time)
   "Seeks to the position time (in seconds) within the current song. If prefixed
   by '+' or '-', then the time is relative to the current playing position."
-  (declare (type unsigned-byte time))
-  (send-command socket "seekcur" (the unsigned-byte time)))
+  (send-command socket "seekcur" time))
 
 (defun playback-stop (socket)
   "Stops playing."
